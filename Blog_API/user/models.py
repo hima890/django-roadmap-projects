7167ac9f7bc114374c84ce5e1c9ecf1e04538f38
hima890 <hfibrahim90@gmail.com>
2025-02-27 13:12:20 +0200

    feat add user model

--- Blog_API/user/models.py
+++ Blog_API/user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
