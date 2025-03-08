493c6ae3522924ba3bf97a6ecc3cf0eba0cac909
hima890 <hfibrahim90@gmail.com>
2025-03-08 13:24:02 +0200

    feat: connect signal handlers on application ready

--- Blog_API/user/apps.py
+++ Blog_API/user/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user'
+
+    def ready(self):
+        """
+        This method is called when the Django application is ready.
+        It imports the signals module to ensure that signal handlers
+        are connected when the application starts.
+        """
+        
+        import user.signals
