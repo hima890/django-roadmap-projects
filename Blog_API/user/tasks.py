27e7af56c477ba135e525c0deeb64fe9988eae7f
hima890 <hfibrahim90@gmail.com>
2025-03-08 12:58:18 +0200

    feat create new task for sending email

--- Blog_API/user/tasks.py
+++ Blog_API/user/tasks.py
@@ -1,6 +1,8 @@
-from celery import shared_task
 from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
 from django.utils.timezone import now
+from celery import shared_task
+from .utility import send_email_with_attachments
+
 
 
 @shared_task
@@ -19,3 +21,20 @@ def clean_expired_blacklisted_tokens():
     count = expired_tokens.count()
     expired_tokens.delete()
     return "Deleted {} expired blacklisted tokens.".format(count)
+
+
+@shared_task
+def send_email_task(subject, template_name, context, recipient_list, attachments=None):
+    """
+    Sends an email with the specified subject, template, context, and recipient list, optionally including attachments.
+    Args:
+        subject (str): The subject of the email.
+        template_name (str): The name of the email template to use.
+        context (dict): A dictionary of context variables to render the template.
+        recipient_list (list): A list of recipient email addresses.
+        attachments (list, optional): A list of file attachments to include in the email. Defaults to None.
+    Returns:
+        bool: True if the email was sent successfully, False otherwise.
+    """
+    
+    return send_email_with_attachments(subject, template_name, context, recipient_list, attachments)
